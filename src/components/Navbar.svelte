c6e873a38b8ffa0bfdc12a4a349ac6885f8cfc72
Oliver Abreu <oabreu@extron.com>
2023-04-30 13:14:20 -0400

    adds logo

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -13,13 +13,13 @@
     <div class="flex items-center justify-center w-full">
       <NavBrand style="display: flex; flex-grow: 1;" href="/">
         <img
-          src="https://flowbite.com/docs/images/logo.svg"
+          src="/src/assets/HEAR-Journal-Online.svg"
           class="mr-3 h-6 sm:h-9"
-          alt="Flowbite Logo"
+          alt="HEAR Journal Logo"
         />
         <span
           class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-          >Flowbite</span
+          >Journal</span
         >
       </NavBrand>
       <DarkMode
